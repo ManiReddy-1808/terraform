0f71cdf335200e075093f6896bc3597fd7c7d921
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-25 22:33:07 +0530

    Added new code

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -3,8 +3,49 @@ resource "aws_instance" "example" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
+  # self is the special variable
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} > inventory.ini"
+  }
+  provisioner "local-exec" {
+    command = "echo script-2"    
+  } 
+  provisioner "local-exec" {
+    command = "exit 1"
+    on_failure = continue # Ignore the error and continue
+  }
+  provisioner "local-exec" {
+    when = destroy
+    command = "echo 'deleting the instance'"    
+  }
+  provisioner "local-exec" {
+    when    = destroy
+    command = "echo > inventory.ini"
+  }
+
+  connection {
+    type     = "ssh"
+    user     = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec" { # Before running this provisioner, takes connection block to connect to the instance and then run the commands mentioned in the inline block. Inside the server.
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo systemctl stop nginx" # Better to stop the service before deleting the instance to avoid any issues.
+    ]
+    when = destroy
+  }
+
   tags = {
-    Name = "terraform-state-demo"
+    Name = "provisioners-demo"
     Project = "roboshop"
   }
 }
